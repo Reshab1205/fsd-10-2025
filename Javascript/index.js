aeb99a60e174293209708bdd4356e0b4d585f930
Reshab Kumar <rkmodi12051998@gmail.com>
2026-02-02 17:24:13 +0530

    Javascript Intro

---
+++ Javascript/index.js
@@ -0,0 +1,43 @@
+// console.log(10 + 10)
+// console.log(2 * 2)
+// console.log(4 / 2)
+// console.log(5 - 13)
+// console.log(4 / 0)
+// console.log('Reshab Kumar')
+// console.log("Reshab Kumar")
+// console.log(`Reshab Kumar`)
+
+// var a = 10
+// let b = 'Reshab'
+// const c = true
+// const d = [1,2,3,4]
+
+
+// console.log(a)
+// console.log(b)
+// console.log(c)
+// console.log(d)
+
+// console.log(a,b,c,d)
+
+
+// var a = 16
+// var a = 10
+// // a = 12
+// // a= 14
+// console.log(a)
+
+// let b;
+// let b = 10;
+// // let b
+// b= 12;
+// b = 17
+// b = 19
+// console.log(b)
+
+// const c = 10
+// // const c = 12;
+// console.log(c)
+
+
+
