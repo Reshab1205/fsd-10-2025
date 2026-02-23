b1d62074c67e99cfefba0bcf3835fecb065a7c6a
Reshab Kumar <rkmodi12051998@gmail.com>
2026-02-23 15:21:27 +0530

    functions

--- Javascript/Function.js
+++ Javascript/Function.js
@@ -33,25 +33,58 @@
 
 // sayhello()
 
-const x = parseInt(prompt("Enter first value: "));
-const y = parseInt(prompt("Enter 2nd value: "));
+// const x = parseInt(prompt("Enter first value: "));
+// const y = parseInt(prompt("Enter 2nd value: "));
 
-// const add = () => {
+// // const add = () => {
 
+// // }
+
+// function add(a, b) {
+//   return a + b;
 // }
 
-function add(a, b) {
-  return a + b;
+// function sub(a, b) {
+//   return a - b;
+// }
+
+// function mul(a, b) {
+//   return a * b;
+// }
+
+// function div(a, b) {
+//   return a / b;
+// }
+
+// function addGst(amt, tax) {
+//   const gst = (amt * tax) / 100 
+//   const totalAmt = amt + gst
+//   return totalAmt
+// }
+
+// console.log(addGst(1000, 28))
+
+
+// const products = [
+//   {
+//   product_name:"Shoes",
+//   product_mrp: 2000,
+//   product_gst:18
+// },
+// {
+//    product_name:"Shoes",
+//   product_mrp: 2000,
+//   product_gst:18
+// }
+// ]
+
+function EmiCalculator(amt, int, time) {
+  const intAmt = (amt * int * 1) / 100 
+  const totalInt = intAmt * time
+  const totalAmt = amt + totalInt
+  const totalMonths = time * 12;
+  const emi = totalAmt / totalMonths;
+  return emi
 }
 
-function sub(a, b) {
-  return a - b;
-}
-
-function mul(a, b) {
-  return a * b;
-}
-
-function div(a, b) {
-  return a / b;
-}
+console.log(EmiCalculator(1000000, 10, 10))
--- Javascript/index.js
+++ Javascript/index.js
@@ -1,378 +1,414 @@
-// console.log(10 + 10)
-// console.log(2 * 2)
-// console.log(4 / 2)
-// console.log(5 - 13)
-// console.log(4 / 0)
-// console.log('Reshab Kumar')
-// console.log("Reshab Kumar")
-// console.log(`Reshab Kumar`)
+// // console.log(10 + 10)
+// // console.log(2 * 2)
+// // console.log(4 / 2)
+// // console.log(5 - 13)
+// // console.log(4 / 0)
+// // console.log('Reshab Kumar')
+// // console.log("Reshab Kumar")
+// // console.log(`Reshab Kumar`)
 
-// var a = 10
-// let b = 'Reshab'
-// const c = true
-// const d = [1,2,3,4]
+// // var a = 10
+// // let b = 'Reshab'
+// // const c = true
+// // const d = [1,2,3,4]
 
-// console.log(a)
-// console.log(b)
-// console.log(c)
-// console.log(d)
+// // console.log(a)
+// // console.log(b)
+// // console.log(c)
+// // console.log(d)
 
-// console.log(a,b,c,d)
+// // console.log(a,b,c,d)
 
-// var a = 16
-// var a = 10
-// // a = 12
-// // a= 14
-// console.log(a)
+// // var a = 16
+// // var a = 10
+// // // a = 12
+// // // a= 14
+// // console.log(a)
 
-// let b;
-// let b = 10;
-// // let b
-// b= 12;
-// b = 17
-// b = 19
-// console.log(b)
+// // let b;
+// // let b = 10;
+// // // let b
+// // b= 12;
+// // b = 17
+// // b = 19
+// // console.log(b)
 
-// const c = 10
-// // const c = 12;
-// console.log(c)
+// // const c = 10
+// // // const c = 12;
+// // console.log(c)
 
-// const data = 5;
-// let x;
+// // const data = 5;
+// // let x;
 
-// function something() {
-// let c = 10 ;
-// data = c
+// // function something() {
+// // let c = 10 ;
+// // data = c
 
-// }
+// // }
 
-// c
+// // c
 
-// function doit() {
-//     console.log(x)
-// }
+// // function doit() {
+// //     console.log(x)
+// // }
 
-// const a = 'Reshab'
-// const b = 'Kumar'
-// const c = a - b;
-// console.log(c)
+// // const a = 'Reshab'
+// // const b = 'Kumar'
+// // const c = a - b;
+// // console.log(c)
 
-// const a = ''
-// const d = "Kumar"
-// const e = `Sah`
-// const b = 10.76
-// const c = true
-// let g = null;
-// let y = g;
-// let x = y
+// // const a = ''
+// // const d = "Kumar"
+// // const e = `Sah`
+// // const b = 10.76
+// // const c = true
+// // let g = null;
+// // let y = g;
+// // let x = y
 
-// const data = String('Reshab')
-// const num = Number('Reshab')
+// // const data = String('Reshab')
+// // const num = Number('Reshab')
 
-// console.log(typeof(num))
-// console.log(num)
+// // console.log(typeof(num))
+// // console.log(num)
 
-// const a = NaN
-// console.log(typeof(a))
+// // const a = NaN
+// // console.log(typeof(a))
 
-// const x = 10;
+// // const x = 10;
 
-// const y = 12;
+// // const y = 12;
 
-// if(x <= y) {
-//     console.log('Hello')
-// } else {
-//     console.log('BYe')
-// }
+// // if(x <= y) {
+// //     console.log('Hello')
+// // } else {
+// //     console.log('BYe')
+// // }
 
-// const data = prompt('Enter the value : ')
-// console.log(data)
+// // const data = prompt('Enter the value : ')
+// // console.log(data)
 
-// 1) Positive or Negative: Write a program that takes a number and prints
-// whether it is "positive," "negative," or "zero".
-// 2)  Even or Odd: Use the remainder operator (%) to determine
-//  if a number is even or odd.
-// 3) Voting Eligibility: Create a script that checks
-//  if a person's age is 18 or older. If so, print "Old enough to vote!";
-//  otherwise, print "Not old enough to vote.".
-// 4) Greater of Two: Write a script that compares
-// two numbers and logs the larger value to the console.
-// 5) Passing Grade: Define a variable score.
-// If the score is 50 or higher, print "Passed"; otherwise, print "Failed".
+// // 1) Positive or Negative: Write a program that takes a number and prints
+// // whether it is "positive," "negative," or "zero".
+// // 2)  Even or Odd: Use the remainder operator (%) to determine
+// //  if a number is even or odd.
+// // 3) Voting Eligibility: Create a script that checks
+// //  if a person's age is 18 or older. If so, print "Old enough to vote!";
+// //  otherwise, print "Not old enough to vote.".
+// // 4) Greater of Two: Write a script that compares
+// // two numbers and logs the larger value to the console.
+// // 5) Passing Grade: Define a variable score.
+// // If the score is 50 or higher, print "Passed"; otherwise, print "Failed".
 
-// O+ --> 90 to 100
-// A+ --> 85 to 90
-// A --> 80 to 85
-// B+ --> 75 to 80
-// B ---> 70 to 75
-// C+ --> 65 to 70
-// C --> 60 to 65
-// D+ --> 55 to 60
-// D --> 50 to 55
-// E --> less than 50
+// // O+ --> 90 to 100
+// // A+ --> 85 to 90
+// // A --> 80 to 85
+// // B+ --> 75 to 80
+// // B ---> 70 to 75
+// // C+ --> 65 to 70
+// // C --> 60 to 65
+// // D+ --> 55 to 60
+// // D --> 50 to 55
+// // E --> less than 50
 
-// less than 0  --> Enter correct Value
+// // less than 0  --> Enter correct Value
 
-// const data = prompt('Enter the number')
+// // const data = prompt('Enter the number')
 
-// if(data > 90 && data <=100) {
-//     console.log('O+')
-// } else if(data > 85 && data <=90) {
-//     console.log('A+')
-// } else if(data > 80 && data <=85) {
-//     console.log('A')
-// } else if(data > 75 && data <=80) {
-//     console.log('B+')
-// } else if(data > 70 && data <=75) {
-//     console.log('B')
-// } else if(data > 65 && data <=70) {
-//     console.log('C+')
-// } else if(data > 60 && data <=65) {
-//     console.log('C')
-// } else if(data > 55 && data <=60) {
-//     console.log('D+')
-// } else if(data > 50 && data <=55) {
-//     console.log('D')
-// } else if (data <= 50 && data >= 0) {
-//     console.log('Fail')
-// } else {
-//     console.log('Enter correct value')
-// }
+// // if(data > 90 && data <=100) {
+// //     console.log('O+')
+// // } else if(data > 85 && data <=90) {
+// //     console.log('A+')
+// // } else if(data > 80 && data <=85) {
+// //     console.log('A')
+// // } else if(data > 75 && data <=80) {
+// //     console.log('B+')
+// // } else if(data > 70 && data <=75) {
+// //     console.log('B')
+// // } else if(data > 65 && data <=70) {
+// //     console.log('C+')
+// // } else if(data > 60 && data <=65) {
+// //     console.log('C')
+// // } else if(data > 55 && data <=60) {
+// //     console.log('D+')
+// // } else if(data > 50 && data <=55) {
+// //     console.log('D')
+// // } else if (data <= 50 && data >= 0) {
+// //     console.log('Fail')
+// // } else {
+// //     console.log('Enter correct value')
+// // }
 
-// if(data > 0) {
-//     console.log('Positive')
-// } else if (data == 0) {
-//     console.log('Zero')
-// } else {
-//     console.log('Negative')
-// }
+// // if(data > 0) {
+// //     console.log('Positive')
+// // } else if (data == 0) {
+// //     console.log('Zero')
+// // } else {
+// //     console.log('Negative')
+// // }
 
-// console.log(arr.length)
-// const str = ["Reshab", "Kumar"]
-// const mix = [1,2,"Reshab", true]
+// // console.log(arr.length)
+// // const str = ["Reshab", "Kumar"]
+// // const mix = [1,2,"Reshab", true]
 
-// i = 0
-// i = i + 1
+// // i = 0
+// // i = i + 1
 
-// for(let i = 0; i < arr.length; i++) {
-//     console.log(arr[i])
-// }
+// // for(let i = 0; i < arr.length; i++) {
+// //     console.log(arr[i])
+// // }
 
-// for(let i = arr1.length - 1; i >= 0; i--) {
-//     console.log(arr1[i])
-// }
+// // for(let i = arr1.length - 1; i >= 0; i--) {
+// //     console.log(arr1[i])
+// // }
 
-//     const arr = [1,2,3,4,5]
-// // const arr1 = ["Reshab", "Kumar", 27, "Vaishali", "Bihar", 844125]
+// //     const arr = [1,2,3,4,5]
+// // // const arr1 = ["Reshab", "Kumar", 27, "Vaishali", "Bihar", 844125]
 
-// arr.forEach((item) => {
-//      item = item * 2
-//     console.log(item)
-// })
-// for(x in arr1) {
-//     console.log('x', x)
-// }
+// // arr.forEach((item) => {
+// //      item = item * 2
+// //     console.log(item)
+// // })
+// // for(x in arr1) {
+// //     console.log('x', x)
+// // }
 
-// for(y of arr1) {
-//     console.log('y', y)
-// }
+// // for(y of arr1) {
+// //     console.log('y', y)
+// // }
 
-// const arr = [1,2,3,4,5]
+// // const arr = [1,2,3,4,5]
 
-// for(let i = arr.length - 1; i >= 0; i--) {
-//     console.log(arr[i])
-// }
+// // for(let i = arr.length - 1; i >= 0; i--) {
+// //     console.log(arr[i])
+// // }
 
-// for(let i in arr) {
-//     console.log(arr[i])
-// }
+// // for(let i in arr) {
+// //     console.log(arr[i])
+// // }
 
-// for( let i of arr) {
-//     console.log(i)
-// }
+// // for( let i of arr) {
+// //     console.log(i)
+// // }
 
-// arr.forEach((item) => {
-//     console.log(item)
-// })
+// // arr.forEach((item) => {
+// //     console.log(item)
+// // })
 
-// const msg = 'Hello'
+// // const msg = 'Hello'
 
-// for(i = 0; i < 5; i++) {
-//     console.log(msg)
-// }
+// // for(i = 0; i < 5; i++) {
+// //     console.log(msg)
+// // }
 
-// const arr1 = ["Kumar", "Ravi", 27, true]
-// const arr = ["Reshab", "Kumar", 27, true]
+// // const arr1 = ["Kumar", "Ravi", 27, true]
+// // const arr = ["Reshab", "Kumar", 27, true]
 
-// console.log(arr[2])
+// // console.log(arr[2])
 
-// const data = [1,2,3, "Reshab", {fname: "Reshab", lname: "Kumar", age:27, isActive:true}]
-// console.log(data)
-// console.log(typeof(arr))
-// console.log(typeof(obj))
+// // const data = [1,2,3, "Reshab", {fname: "Reshab", lname: "Kumar", age:27, isActive:true}]
+// // console.log(data)
+// // console.log(typeof(arr))
+// // console.log(typeof(obj))
 
-// const age = 27
-// age = 28
-// console.log(age)
+// // const age = 27
+// // age = 28
+// // console.log(age)
 
-// const ar = [1,2,3,4,5]
-// ar[2] = 9
-// console.log(ar)
-// obj.age = 28
-// console.log(obj)
+// // const ar = [1,2,3,4,5]
+// // ar[2] = 9
+// // console.log(ar)
+// // obj.age = 28
+// // console.log(obj)
 
-// for(i in obj) {
-//     console.log(i)
-// }
+// // for(i in obj) {
+// //     console.log(i)
+// // }
 
-// for(i in obj) {
-//     console.log(obj[i])
-// }
+// // for(i in obj) {
+// //     console.log(obj[i])
+// // }
 
+// // function add(a,b) {
+// //     const c = a + b;
+// //     console.log(c)
+// //    return add(5,6) dont try this
+// // }
+// // add()
 
+// // const fname = "Reshab"
 
+// // const data = `Hello ${fname}`
+// // console.log(data)
 
-// function add(a,b) {
-//     const c = a + b;
-//     console.log(c)
-//    return add(5,6) dont try this 
-// }
-// add()
+// // const obj = {
+// //   fname: "Reshab",
+// //   lname: "Kumar",
+// //   age: 27,
+// //   isActive: true,
+// //   hello: function greet() {
+// //     console.log(`Hello ${obj.fname + " " + obj.lname}
+// //         my age is ${obj.age} and is it ${obj.isActive}`)
+// //   }
+// // };
+// // console.log(obj.fname)
+// // obj.hello()
 
+// // const obj = {
+// //   fname: "Reshab",
+// //   lname: "Kumar",
+// //   age: 27,
+// //   isActive: false,
+// //   hello: function greet() {
+// //     console.log(`Hello ${this.fname + " " + this.lname}
+// //         my age is ${this.age} and is it ${this.isActive}`)
+// //   },
+// //   getFullName: function fullName() {
+// //     console.log(this.fname + this.lname)
+// //   }
+// // };
 
-// const fname = "Reshab"
+// // // console.log(typeof(obj[keys]))
+// // for(i in obj) {
+// //   if(typeof(obj[i]) !== 'function' && typeof(obj[i]) !== 'boolean') {
+// //     console.log(obj[i])
+// //   }
+// // }
 
-// const data = `Hello ${fname}`
-// console.log(data)
+// // for( i in obj) {
+// //   if( typeof(obj[i]) === 'string') {
+// //     console.log(i)
+// //   }
+// // }
 
+// // obj.hello()
 
-// const obj = {
-//   fname: "Reshab",
-//   lname: "Kumar",
-//   age: 27,
-//   isActive: true,
-//   hello: function greet() { 
-//     console.log(`Hello ${obj.fname + " " + obj.lname} 
-//         my age is ${obj.age} and is it ${obj.isActive}`)
-//   }
+// // function hii(name, age) {
+// //   console.log(name + " " + age)
+// // }
+
+// // hii(obj.fname, obj.age)
+
+// // console.log(obj.getFullName)
+// // for(i in obj) {
+// //   console.log(i)
+// // }
+
+// // const obj = new Object({
+// //   fname: "Reshab",
+// //   lname: "Kumar",
+// //   age: 27,
+// //   isActive: false,
+// //   hello: function greet() {
+// //     console.log(`Hello ${this.fname + " " + this.lname}
+// //         my age is ${this.age} and is it ${this.isActive}`)
+// //   },
+// //   getFullName: function fullName() {
+// //     console.log(this.fname + this.lname)
+// //   }
+// // })
+
+// // Object.seal(obj)
+// // obj.isActive = true
+// // obj.city = "Bhopal"
+
+// // console.log(Object.isSealed(obj))
+// // console.log(obj)
+
+// // console.log(obj)
+
+// // create an obj where it will store fname,
+// // lname , greeting msg and create a function
+// // inside obj which will display greet msg
+
+// // const x = 'Reshab'
+
+// // console.log(`Hello, How are you ${x} `)
+
+// // const obj = {
+// //   fname: "Reshab",
+// //   lname: "Kumar",
+// //   msg: "Thank You for Coming",
+// //   isActive:true,
+// //   greet: function sayGreet() {
+// //     // c =
+// //     console.log(`${this.fname}  ${this.lname}, ${this.msg}`)
+// //   },
+// //   hello: function sayHello() {
+// //     console.log('Hello')
+// //   },
+// //   isSeal: false
+// // }
+
+// // for( let i in obj) {
+// //   if(typeof(obj[i]) !== 'function') {
+// //     console.log('first iteration',i)
+// //     if(typeof(obj[i]) !== 'boolean') {
+// //       console.log(i)
+// //     }
+// //   }
+// // }
+
+// // for( let i in obj) {
+// //   if(typeof(obj[i]) !== 'function' && typeof(obj[i]) !== 'boolean') {
+// //       console.log(i)
+// //   }
+// // }
+
+// // console.log(obj.fname)
+// // Object.freeze(obj)
+// // Object.seal(obj)
+// // obj.city = 'Vaishali'
+// // console.log(obj)
+
+// // const x = Object.values(obj)
+// // console.log(x)
+
+// const person = {
+//   firstName: "Reshab",
+//   lastName: "Kumar",
+//   fullName: function () {
+//     return this.firstName + " " + this.lastName;
+//   },
 // };
-// console.log(obj.fname)
-// obj.hello()
 
-// const obj = {
-//   fname: "Reshab",
-//   lname: "Kumar",
-//   age: 27,
-//   isActive: false,
-//   hello: function greet() { 
-//     console.log(`Hello ${this.fname + " " + this.lname} 
-//         my age is ${this.age} and is it ${this.isActive}`)
+// // function keys(x) {
+// //     console.log(Object.entries(x))
+// // }
+
+// // keys(person)
+
+// // function hasProperty (x,y) {
+// //     console.log(x.hasOwnProperty(y))
+// // }
+
+// // hasProperty(person, 'age')
+
+// // console.log(person.hasOwnProperty('lastName'))
+// // Object.has
+
+// // console.log(person.fullName())
+
+// const arr = [1, 2, 3, 4, 5];
+// const arr1 = [6, 7, 8, 9, 10];
+
+// // [1,2,3,4,5,6,7,8,9,10]
+// arr.concat(arr1);
+
+// const d = arr;
+
+// // const arr2 = [...arr, ...arr1]
+// console.log(arr);
+
+// const obj = [
+//   {
+//     S_no: 1,
+//     Name: "Reshab",
+//     Roll_no: 1,
 //   },
-//   getFullName: function fullName() {
-//     console.log(this.fname + this.lname)
-//   }
-// };
-
-
-
-// // console.log(typeof(obj[keys]))
-// for(i in obj) {
-//   if(typeof(obj[i]) !== 'function' && typeof(obj[i]) !== 'boolean') {
-//     console.log(obj[i])
-//   }
-// }
-
-// for( i in obj) {
-//   if( typeof(obj[i]) === 'string') {
-//     console.log(i)
-//   }
-// }
-
-// obj.hello()
-
-// function hii(name, age) {
-//   console.log(name + " " + age) 
-// }
-
-// hii(obj.fname, obj.age)
-
-
-// console.log(obj.getFullName)
-// for(i in obj) {
-//   console.log(i)
-// }
-
-
-
-// const obj = new Object({
-//   fname: "Reshab",
-//   lname: "Kumar",
-//   age: 27,
-//   isActive: false,
-//   hello: function greet() { 
-//     console.log(`Hello ${this.fname + " " + this.lname} 
-//         my age is ${this.age} and is it ${this.isActive}`)
+//   {
+//     S_no: 2,
+//     Name: "Baibhav",
+//     Roll_no: 2,
 //   },
-//   getFullName: function fullName() {
-//     console.log(this.fname + this.lname)
-//   }
-// })
-
-
-// Object.seal(obj)
-// obj.isActive = true
-// obj.city = "Bhopal"
-
-// console.log(Object.isSealed(obj))
-// console.log(obj)
-
-// console.log(obj)
-
-
-// create an obj where it will store fname, 
-// lname , greeting msg and create a function 
-// inside obj which will display greet msg 
-
-// const x = 'Reshab'
-
-// console.log(`Hello, How are you ${x} `)
-
-// const obj = {
-//   fname: "Reshab",
-//   lname: "Kumar",
-//   msg: "Thank You for Coming",
-//   isActive:true,
-//   greet: function sayGreet() {
-//     // c = 
-//     console.log(`${this.fname}  ${this.lname}, ${this.msg}`)
-//   },
-//   hello: function sayHello() {
-//     console.log('Hello')
-//   },
-//   isSeal: false
-// }
-
-// for( let i in obj) {
-//   if(typeof(obj[i]) !== 'function') {
-//     console.log('first iteration',i)
-//     if(typeof(obj[i]) !== 'boolean') {
-//       console.log(i)
-//     }
-//   }
-// }
-
-// for( let i in obj) {
-//   if(typeof(obj[i]) !== 'function' && typeof(obj[i]) !== 'boolean') {
-//       console.log(i)
-//   }
-// }
-
-// console.log(obj.fname)
-// Object.freeze(obj)
-// Object.seal(obj)
-// obj.city = 'Vaishali'
-// console.log(obj)
-
-// const x = Object.values(obj)
-// console.log(x)
-
+// ];
