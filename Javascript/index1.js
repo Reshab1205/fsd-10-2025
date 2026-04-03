7de143e5e4170d57691104e28327fae6c499306a
Reshab Kumar <rkmodi12051998@gmail.com>
2026-04-03 17:13:29 +0530

    revision

---
+++ Javascript/index1.js
@@ -0,0 +1,100 @@
+// console.time()
+// const arr = [1,2,[3,4],5,6,[7,8,[11,12]],9,10]
+// const result = arr.flat(Infinity)
+// console.table(result)
+// console.timeEnd()
+// console.error('Error')
+
+
+// [1,2,3,4,5,6,7,8,9,10]
+
+//type coercion
+// console.log([] == ![])
+// console.log("" == !null)
+
+
+//Doubts 01/04/26
+//Object & Object Methods
+//Array & Loops
+//DOM related to project
+
+// const obj = {
+//     std_1: "Reshab",
+//     std_2:"Sahil",
+//     std_3: "Sumit",
+//     std_4: "Hassan"
+// }
+
+// for(let i in obj) {
+//     console.log(obj[i])
+// }
+
+// obj.array.forEach(element => {
+    
+// });
+
+// for(let i of obj) {
+//     console.log(obj[i])
+// }
+// for(let i = 0)
+
+
+// const arr = ["Reshab", "Sahil", "Sumit", "Hassan"]
+// arr.for 
+// console.log(arr.length)
+
+//Traditional For loop
+// for(let i = 0; i < arr.length; i++) {
+//     console.log(arr[i])
+// }
+
+// for(let i in arr) {
+//     console.log(arr[i])
+// }
+
+// for(let i of arr) {
+//     console.log(i)
+// }
+
+// arr.forEach((i,a) => {
+//     console.log(i)
+//     console.log(a)
+// })
+
+// arr.forEach((data, index) => {
+//     console.log(data)
+//     console.log(index)
+// })
+
+
+
+// for()
+//     forin
+// forof 
+// forEach 
+
+// obj['std_5'] = 'Rahul'
+// // console.log(obj)
+
+// Object.freeze(obj)
+// obj['std_1'] = 'Amit'
+// obj.std_6 = 'Anuj'
+// console.log(obj)
+
+
+
+// arr.splice(0,2)
+// arr.push("Uday")
+// arr.shift()
+// arr.unshift("Uday")
+// arr.pop()
+// console.log(arr)
+
+// Object.seal(arr)
+// arr[0] = 'Amit'
+// arr.push('Uday')
+// console.log( arr[6].std_4)
+// console.log( typeof obj.std_4)
+
+
+
