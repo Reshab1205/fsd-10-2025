8544fe136f96a684b9d350829bdddd93e8a636db
Reshab Kumar <rkmodi12051998@gmail.com>
2026-04-08 17:15:35 +0530

    call,apply,bind

--- Javascript/Async.js
+++ Javascript/Async.js
@@ -25,13 +25,69 @@
 // Promise.resolve().then(()=> console.log('promise'))
 // console.log('Bye')
 
-function hello() {
-  console.log("Hello");
-}
+// function hello() {
+//   console.log("Hello");
+// }
 
-function greet() {
-  console.log("How are you");
-  hello();
-}
+// function greet() {
+//   console.log("How are you");
+//   hello();
+// }
 
-greet()
+// greet()
+
+// function hello() {
+//     console.log('Hello')
+// }
+
+// let count = 0
+// const x = setInterval(() => {
+//     hello();
+//     count++
+//     if(count >= 5) {
+//         clearInterval(x)
+//         console.log('Function runs 5 times')
+//     }
+// },1000)
+// console.log('Bye')
+
+// setTimeout(() => {
+//     hello()
+// }, 1000)
+
+// setInterval(hello, 1000)
+// clearInterval(x)
+
+// hello()
+
+// call apply bind
+
+const data = {
+  fname: "Reshab",
+  greet: function (age) {
+    console.log("My Name is " + this.fname + " and my age is " +  age);
+  },
+};
+const user = [
+  { fname: "Ravi", age: 28 },
+  { fname: "Baibhav", age: 27 },
+  { fname: "Shubham", age: 29 },
+  { fname: "Avinash", age: 28 },
+];
+
+const user1 = { fname: "Ravi"}
+
+// data.greet.apply(user1, [28, "Vaishali", true])
+// const result = data.greet.bind(user1, [28, "Vaishali", true])
+// result()
+
+// for(let i of user) {
+//     // console.log(i)
+//     // data.greet.apply(i);
+//    const result =  data.greet.bind(i)
+//    result()
+// }
+
+for( let i of user) {
+    data.greet.apply(i, [i.age])
+}
